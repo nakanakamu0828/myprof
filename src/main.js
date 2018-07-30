bf89328dae0c75459fac32084ff840bc107df19d
Yuki Nakamura <nakamura.yuuki.0606@gmail.com>
2018-07-30 23:37:09 +0800

    feature: google analytiscを追加

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue';
 import Buefy from 'buefy';
+import VueAnalytics from 'vue-analytics';
 import App from './App.vue';
 import './assets/scss/app.scss';
 import router from './router';
 import store from './store';
 
-Vue.use(Buefy);
 
+Vue.use(VueAnalytics, {
+  id: 'UA-113488529-4',
+  router,
+});
+
+Vue.use(Buefy);
 Vue.config.productionTip = false;
 
 new Vue({
