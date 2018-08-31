6c59394a4fa1d4d5fb7d3ee066d1a3b92f9e72d5
Yuki Nakamura <nakamura.yuuki.0606@gmail.com>
2018-08-31 17:02:38 +0800

    refactor: スタイル調整

--- src/components/Home/Socials.vue
+++ src/components/Home/Socials.vue
@@ -8,7 +8,7 @@
       </h2>
       <p class="subtitle has-text-white is-7">ソーシャル</p>
     </div>
-    <div class="columns is-centered is-multiline m-t-50">
+    <div class="columns is-centered is-multiline m-t-50 social-links">
       <a href="https://twitter.com/nakanakamu0828" target="_blank" class="column is-4">
         <div class="box has-text-centered has-text-white is-bg-twitter" style="padding: 3rem;">
           <i class="fab fa-twitter fa-3x"></i>
@@ -34,7 +34,7 @@
         </div>
       </a>
 
-      <a href="https://instagram.com/____nakamu____" target="_blank" class="jsx-3619482712 column is-one-third social-links">
+      <a href="https://instagram.com/____nakamu____" target="_blank" class="column is-4">
         <div class="box has-text-centered has-text-white is-bg-instagram" style="padding: 3rem;">
           <i class="fab fa-instagram fa-3x"></i>
           <p class="subtitle m-t-10">
@@ -54,5 +54,12 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+.social-links {
+  & > a:hover {
+      transform: scale(1.025);
+  }
+}
+
 </style>
