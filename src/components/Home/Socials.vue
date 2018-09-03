02754319fcc0f52d53df261da2fd9b0a4eac8ad5
Yuki Nakamura <nakamura.yuuki.0606@gmail.com>
2018-09-03 10:27:07 +0800

    refactor: リンクの調整

--- src/components/Home/Socials.vue
+++ src/components/Home/Socials.vue
@@ -9,39 +9,38 @@
       <p class="subtitle has-text-white is-7">ソーシャル</p>
     </div>
     <div class="columns is-centered is-multiline m-t-50 social-links">
-      <a href="https://twitter.com/nakanakamu0828" target="_blank" class="column is-4">
-        <div class="box has-text-centered has-text-white is-bg-twitter" style="padding: 3rem;">
+      <div class="column is-4">
+        <a href="https://twitter.com/nakanakamu0828" target="_blank" class="box has-text-centered has-text-white is-bg-twitter" style="padding: 3rem;">
           <i class="fab fa-twitter fa-3x"></i>
           <p class="subtitle m-t-10">
             Twitter
           </p>
-        </div>
-      </a>
-      <a href="https://github.com/nakanakamu0828" target="_blank" class="column is-4">
-        <div class="box has-text-centered has-text-white is-bg-github" style="padding: 3rem;">
+        </a>
+      </div>
+      <div class="column is-4">
+        <a href="https://github.com/nakanakamu0828" target="_blank" class="box has-text-centered has-text-white is-bg-github" style="padding: 3rem;">
           <i class="fab fa-github fa-3x"></i>
           <p class="subtitle m-t-10">
             GitHub
           </p>
-        </div>
-      </a>
-      <a href="https://medium.com/@nakamu" target="_blank" class="column is-4">
-        <div class="box has-text-centered has-text-white is-bg-medium" style="padding: 3rem;">
+        </a>
+      </div>
+      <div class="column is-4">
+        <a href="https://medium.com/@nakamu" target="_blank" class="box has-text-centered has-text-white is-bg-medium" style="padding: 3rem;">
           <i class="fab fa-medium-m fa-3x"></i>
           <p class="subtitle m-t-10">
             Medium
           </p>
-        </div>
-      </a>
-
-      <a href="https://instagram.com/____nakamu____" target="_blank" class="column is-4">
-        <div class="box has-text-centered has-text-white is-bg-instagram" style="padding: 3rem;">
+        </a>
+      </div>
+      <div class="column is-4">
+        <a href="https://instagram.com/____nakamu____" target="_blank" class="box has-text-centered has-text-white is-bg-instagram" style="padding: 3rem;">
           <i class="fab fa-instagram fa-3x"></i>
           <p class="subtitle m-t-10">
             Instagram
           </p>
-        </div>
-      </a>
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -57,8 +56,10 @@ export default {
 <style lang="scss" scoped>
 
 .social-links {
-  & > a:hover {
+  & > div > a:hover {
       transform: scale(1.025);
+      -webkit-box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px #b8b8b8;
+      box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px #b8b8b8;
   }
 }
 
