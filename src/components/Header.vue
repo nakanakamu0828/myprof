ed7f6af03d4b6f6c49d5da0350f07244da693a06
Yuki Nakamura <nakamura.yuuki.0606@gmail.com>
2018-09-16 14:35:33 +0800

    レイアウト調整

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,7 +9,6 @@
           <div class="subtitle is-size-7"> 世界を旅して暮らしたい放浪エンジニア</div>
         </router-link>
 
-
         <span class="navbar-burger burger" :class="{'is-active': drawer}" @click="drawer = !drawer">
           <span></span>
           <span></span>
@@ -18,27 +17,37 @@
       </div>
       <div class="navbar-menu" :class="{'is-active': drawer}">
         <div class="navbar-end">
-          <router-link
-            to="/"
-            class="navbar-item is-active is-text-font-quicksand"
-          >
-            About
-          </router-link>
-          <a
-            href="https://blog.nakamu.life/"
-            class="navbar-item is-text-font-quicksand"
-            target="_blank"
-          >
-            Blog
-          </a>
-          <a
-            href="javascript:void(0)"
-            class="navbar-item is-text-font-quicksand"
-            @click="toggleContactModal"
-          >
-            Contact
-          </a>
 
+          <div class="tabs is-centered m-b-0">
+              <ul>
+                  <li>
+                    <router-link
+                      to="/"
+                      class="is-text-font-quicksand"
+                    >
+                      About
+                    </router-link>
+                  </li>
+                  <li>
+                    <a
+                      href="https://blog.nakamu.life/"
+                      class="is-text-font-quicksand"
+                      target="_blank"
+                    >
+                      Blog
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="javascript:void(0)"
+                      class="is-text-font-quicksand"
+                      @click="toggleContactModal"
+                    >
+                      Contact
+                    </a>
+                  </li>
+              </ul>
+          </div>
         </div>
       </div>
     </div>
@@ -82,8 +91,47 @@ export default {
 
   .navbar-menu {
     & > .navbar-start, & > .navbar-end {
-      a {
-        font-weight: 600;
+      .tabs {
+        & > ul {
+          border: none;
+
+          & > li {
+            & > a {
+              color: #fff;
+              border: none;
+
+              position: relative;
+              display: inline-block;
+
+              &:before, &:after {
+                position: absolute;
+                top: 2rem;
+                content: "";
+                display: inline-block;
+                width: 0;
+                height: 2px;
+                background: #fff;
+                transition: .5s;
+              }
+
+              &:before {
+                left: 50%;
+              }
+              &:after {
+                right: 50%;
+              }
+
+              &:hover {
+                font-weight: 600;
+
+                &:before, &:after {
+                  width: 50%;
+                }
+              }
+            }
+
+          }
+        }
       }
     }
   }
