e5d62e00e179ab8adc59c377c07288e81f9c7d50
nakanakamu0828 <yuuki.nakamura.0828@gmail.com>
2019-01-23 13:16:22 +0800

    デバッグ

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -136,7 +136,7 @@ export default {
     ],
     script: [
       { type: 'text/javascript', src: '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js', async: true},
-      { type: 'text/javascript', innerHTML: '(adsbygoogle = window.adsbygoogle || []).push({google_ad_client: "ca-pub-9693505561732401", enable_page_level_ads: true});' },
+      { type: 'text/javascript', innerHTML: '(adsbygoogle = window.adsbygoogle || []).push({google_ad_client: \'ca-pub-9693505561732401\', enable_page_level_ads: true});' },
     ],
   },
   data: () => ({
